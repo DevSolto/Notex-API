161026746dde0441b942cd75215f6e3511ebb66a
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2024-09-05 15:52:36 +0000

    Validação da rota createUser

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,26 @@
 import { Router } from "express";
 import { createUserService, getUsersServices } from "../services/users";
+import { createUserSchema } from "../schemas/users";
+import { ZodError } from "zod";
 
 export const router = Router();
+
 router.get('/users', async (req, res) => {
     const users = await getUsersServices()
     res.send(users)
 })
 
 router.post('/users', async (req, res) => {
-    const createUserParams = req.body
-    const createdUser = await createUserService(createUserParams)
-    res.send(createdUser)
+    try {
+        const createUserParams = createUserSchema.parse(req.body)
+        const createdUser = await createUserService(createUserParams)
+        res.send(createdUser)
+    } catch (error) {
+        if(error instanceof ZodError){
+            res.status(400).send(error)
+        } else{
+            res.status(500).send(error)
+
+        }
+    }
 })
