0d4690317e1536b27f1d75b344ea3614403f5879
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2024-10-03 15:04:44 -0300

    teste

--- src/schemas/reports.ts
+++ src/schemas/reports.ts
@@ -2,6 +2,6 @@ import { z } from 'zod'
 
 export const createReportSchema = z.object({
     title: z.string().min(1, "O título é obrigatório!"),
-    description: z.string().min(1, "A descrição é obrigatória!")
-    creator
+    description: z.string().min(1, "A descrição é obrigatória!"),
+    creatorId: z.string().cuid()
 })
