2a96360446171df0c47e012dd64177cb0b6ca378
Emmanuel Ferrão <110433258+emmanuelferrao@users.noreply.github.com>
2024-10-03 18:14:04 +0000

    Correcoes de rotas

--- src/schemas/reports.ts
+++ src/schemas/reports.ts
@@ -2,8 +2,12 @@ import { z } from 'zod'
 
 export const createReportSchema = z.object({
     title: z.string().min(1, "O título é obrigatório!"),
-    description: z.string().min(1, "A descrição é obrigatória!"),
-    creatorId: z.string().cuid()
+    description: z.string().min(1, "A descrição é obrigatória!")
+})
+
+export const updateReportSchema = z.object({
+    title: z.string().min(1, "O título é obrigatório!").optional(),
+    description: z.string().min(1, "A descrição é obrigatória!").optional()
 })
 
 export const updateReportSchema = z.object({
